4523a845597e1bf17fdf1c8cc06abd8c68ec7a49
Ofir <ofirfirst@gmail.com>
2023-06-30 23:51:14 +0300

    first one

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
-
+    for y in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
     # Set the footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -22,11 +22,14 @@ for index, row in df.iterrows():
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-
         # Set the footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
+
 
 pdf.output("output.pdf")
